528a0dfe55477aa615ce4f0c90c7eccd781f2505
tiagocsarmento <tiagocsarmento@gmail.com>
2023-10-22 13:44:55 -0300

    Project created.

---
+++ src/test/java/tiago/projectlabAPIdesafio/ProjectLabApiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package tiago.projectlabAPIdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLabApiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
